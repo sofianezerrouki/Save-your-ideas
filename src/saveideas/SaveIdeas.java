f5f16952cc5c3188a537b4713c924a9138dd1eaf
sofiane <sofianeyooung@gmail.com>
2020-10-04 23:07:43 +0100

    change interface

--- src/saveideas/SaveIdeas.java
+++ src/saveideas/SaveIdeas.java
@@ -6,10 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author ASUS
- */
+
 public class SaveIdeas extends Application {
     
     @Override
